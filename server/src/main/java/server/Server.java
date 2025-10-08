84785d000debada464037d443a87644947505bca
Vincent Gardner <vinnyogardner@gmail.com>
2025-10-08 14:56:39 -0600

    Added files for phase 3

---
+++ server/src/main/java/server/Server.java
@@ -0,0 +1,27 @@
+package server;
+
+import io.javalin.*;
+
+public class Server {
+
+    private final Javalin server;
+
+    public Server() {
+        server = Javalin.create(config -> config.staticFiles.add("web"));
+
+        server.delete("db", ctx -> ctx.result("{}"));
+        server.post("user", ctx -> ctx.result())
+
+        // Register your endpoints and exception handlers here.
+
+    }
+
+    public int run(int desiredPort) {
+        server.start(desiredPort);
+        return server.port();
+    }
+
+    public void stop() {
+        server.stop();
+    }
+}
