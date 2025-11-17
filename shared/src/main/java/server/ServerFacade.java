d338efd6804191b2bc15b3de7fa4effb1d3de758
Vincent Gardner <vinnyogardner@gmail.com>
2025-11-17 12:46:03 -0700

    Added petshop facade example and exception as a template edited a couple requests to match chess

---
+++ shared/src/main/java/server/ServerFacade.java
@@ -0,0 +1,103 @@
+package server;
+
+import com.google.gson.Gson;
+import exception.ResponseException;
+import model.*;
+
+import java.net.*;
+import java.net.http.*;
+import java.net.http.HttpRequest.BodyPublisher;
+import java.net.http.HttpRequest.BodyPublishers;
+import java.net.http.HttpResponse.BodyHandlers;
+
+public class ServerFacade {
+    private final HttpClient client = HttpClient.newHttpClient();
+    private final String serverUrl;
+    private String auth;
+
+    public ServerFacade(String url) {
+        serverUrl = url;
+    }
+
+    public AuthData register(UserData user) throws ResponseException {
+        HttpRequest request = buildRequest("POST", "/user", user);
+        HttpResponse<String> response = sendRequest(request);
+        return handleResponse(response, AuthData.class);
+    }
+
+    public void delete() throws ResponseException {
+        HttpRequest request = buildRequest("DELETE", "/", null);
+        HttpResponse<String> response = sendRequest(request);
+        handleResponse(response, null);
+    }
+
+    public AuthData login(UserData user) throws ResponseException {
+        HttpRequest request = buildRequest("POST", "/session", user);
+        HttpResponse<String> response = sendRequest(request);
+        auth = null;
+        return handleResponse(response, AuthData.class);
+    }
+
+    public void logout() throws ResponseException {
+        HttpRequest request = buildRequest("DELETE", "/session", null);
+        HttpResponse<String> response = sendRequest(request);
+        handleResponse(response, null);
+    }
+
+    public String createGame(GameData newGame) throws ResponseException {
+        HttpRequest request = buildRequest("POST", "/game", newGame);
+        HttpResponse<String> response = sendRequest(request);
+        return handleResponse(response, String.class);
+    }
+
+    private HttpRequest buildRequest(String method, String path, Object body) {
+        HttpRequest.Builder request = HttpRequest.newBuilder()
+                .uri(URI.create(serverUrl + path))
+                .method(method, makeRequestBody(body));
+        if (auth != null) {
+            request.setHeader("Authorization", auth);
+        }
+        else if (body != null) {
+            request.setHeader("Content-Type", "application/json");
+        }
+        return request.build();
+    }
+
+    private BodyPublisher makeRequestBody(Object request) {
+        if (request != null) {
+            return BodyPublishers.ofString(new Gson().toJson(request));
+        } else {
+            return BodyPublishers.noBody();
+        }
+    }
+
+    private HttpResponse<String> sendRequest(HttpRequest request) throws ResponseException {
+        try {
+            return client.send(request, BodyHandlers.ofString());
+        } catch (Exception ex) {
+            throw new ResponseException(ResponseException.Code.ServerError, ex.getMessage());
+        }
+    }
+
+    private <T> T handleResponse(HttpResponse<String> response, Class<T> responseClass) throws ResponseException {
+        int status = response.statusCode();
+        if (!isSuccessful(status)) {
+            String body = response.body();
+            if (body != null) {
+                throw ResponseException.fromJson(body);
+            }
+
+            throw new ResponseException(ResponseException.fromHttpStatusCode(status), "other failure: " + status);
+        }
+
+        if (responseClass != null) {
+            return new Gson().fromJson(response.body(), responseClass);
+        }
+
+        return null;
+    }
+
+    private boolean isSuccessful(int status) {
+        return status / 100 == 2;
+    }
+}
