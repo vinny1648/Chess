dec6ecec1cb17c9b02ea03fc32ef92ff4d6987f9
Vincent Gardner <vinnyogardner@gmail.com>
2025-11-18 15:24:23 -0700

    Added register to Client

--- client/src/main/java/ui/ChessClient.java
+++ client/src/main/java/ui/ChessClient.java
@@ -1,6 +1,7 @@
 package ui;
 
 import exception.ResponseException;
+import model.*;
 import server.ServerFacade;
 
 import static ui.ChessClient.PlayerState.*;
@@ -32,13 +33,13 @@ public class ChessClient {
         System.out.print(menu());
 
         Scanner scanner = new Scanner(System.in);
-        var result = "";
+        String result = "";
         while (!result.equals("quit")) {
             if (playerState == UNLOGGED) {
-                System.out.print("\n" + ERASE_SCREEN + username ">>> " + SET_TEXT_COLOR_MAGENTA);
+                System.out.print("\n" + ERASE_SCREEN + ">>> " + SET_TEXT_COLOR_MAGENTA);
             }
             else {
-                System.out.print("\n" + ERASE_SCREEN + ">>> " + SET_TEXT_COLOR_MAGENTA);
+                System.out.print("\n" + ERASE_SCREEN + username + ">>> " + SET_TEXT_COLOR_MAGENTA);
             }
             String line = scanner.nextLine();
 
@@ -46,13 +47,13 @@ public class ChessClient {
                 result = evalInput(line);
                 System.out.print(SET_TEXT_COLOR_GREEN + result);
             } catch (Throwable e) {
-                var msg = e.toString();
+                String msg = e.toString();
                 System.out.print(msg);
             }
         }
         System.out.println();
     }
-    private void evalInput(String line) {
+    private String evalInput(String line) throws ResponseException {
         String[] tokens = line.toLowerCase().split(" ");
         String cmd = (tokens.length > 0) ? tokens[0] : "help";
         String[] params = Arrays.copyOfRange(tokens, 1, tokens.length);
@@ -64,7 +65,7 @@ public class ChessClient {
                 case "quit" -> "quit";
                 default -> help();
             };
-        } else if (playerState = MENU) {
+        } else if (playerState == MENU) {
             return switch (cmd) {
                 case "creategame" -> createGame(params);
                 case "joingame" -> joinGame(params);
@@ -76,6 +77,35 @@ public class ChessClient {
                 default -> help();
             };
         }
+    }
+    private String register(String... params) throws ResponseException{
+        String usrnm = params[0];
+        String password = params[1];
+        String email = params[2];
+        String name = server.register(new UserData(usrnm, password, email));
+        username = name;
+        return "Logged In";
+    }
+    private String login(String... params) {
+
+    }
+    private String createGame(String... params) {
+
+    }
+    private String joinGame(String... params) {
+
+    }
+    private String observe(String... params) {
+
+    }
+    private String listGames() {
+
+    }
+    private String logout() {
+
+    }
+    private String help() {
+
     }
     private String menu() {
         if (playerState == UNLOGGED) {
