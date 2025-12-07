7debb195c96cd0346bbcc95b70ec1e7f2e2f3cd8
Vincent Gardner <vinnyogardner@gmail.com>
2025-12-06 17:48:07 -0700

    Adjusted connection manager to specify specific games

--- server/src/main/java/service/websockethandler/ConnectionManager.java
+++ server/src/main/java/service/websockethandler/ConnectionManager.java
@@ -5,28 +5,33 @@ import org.eclipse.jetty.websocket.api.Session;
 import websocket.messages.ServerMessage;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class ConnectionManager {
-    public final ConcurrentHashMap<Session, Session> connections = new ConcurrentHashMap<>();
+    public final ConcurrentHashMap<Integer, ArrayList<Session>> connections = new ConcurrentHashMap<>();
 
     private final Gson gson = new Gson();
 
     public void add(Session session, Integer gameID) {
-        connections.put(session, session);
+        if (!connections.containsKey(gameID)) {
+            connections.put(gameID, new ArrayList<>());
+        }
+        connections.get(gameID).add(session);
     }
 
-    public void remove(Session session) {
-        connections.remove(session);
+    public void remove(Session session, Integer gameID) {
+        connections.get(gameID).remove(session);
+        if (connections.get(gameID).isEmpty()) {
+            connections.remove(gameID);
+        }
     }
 
-    public void broadcast(Session excludeSession, ServerMessage notification) throws IOException {
+    public void broadcast(Integer gameID, ServerMessage notification) throws IOException {
         String msg = gson.toJson(notification);
-        for (Session c : connections.values()) {
+        for (Session c : connections.get(gameID)) {
             if (c.isOpen()) {
-                if (!c.equals(excludeSession)) {
-                    c.getRemote().sendString(msg);
-                }
+                c.getRemote().sendString(msg);
             }
         }
     }
